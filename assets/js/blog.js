bd4576b26b8dd02bb43e8e431875ff6d97bec5c0
Aerf <ABNUS-aref@hotmail.com>
2024-09-30 09:56:03 -0700

    Add files via upload

---
+++ assets/js/blog.js
@@ -0,0 +1,9 @@
+
+document.writeln(" <div class=\"blog-area ptb-100\"><div class=\"container\"><div class=\"row align-items-center justify-content-center\"> "); 
+document.writeln(" <div class=\"col-xl-4 col-md-6\"><div class=\"blog-single-item style-two\"> "); 
+document.writeln(" <a href=\"single-blog.html\" class=\"d-block blog-img\"><img src=\"assets/images/123.jpg\" alt=\"blog\"></a><ul class=\"ps-0 list-unstyled date\"> "); 
+document.writeln(" <li><span>2025</span></li> "); 
+document.writeln(" <li>جدید</li> "); 
+document.writeln(" </ul> "); 
+document.writeln(" <h3><a href=\"\YouTube.html\">با اساتید علوم غریبه اشنا شوید</a></h3> "); 
+document.writeln(" </div></div> ");
