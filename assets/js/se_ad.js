34020fd925c1d9bf4fabf8776a31110d8b0742cb
Aerf <ABNUS-aref@hotmail.com>
2024-09-30 09:55:17 -0700

    Delete assets/js directory

--- assets/js/se_ad.js
+++
@@ -1,8 +0,0 @@
-document.writeln("  <footer> "); 
-document.writeln("         <div> "); 
-document.writeln("             <i class='bx bx-x'></i> "); 
-document.writeln("             <p>خرید حرز امام جواد با تخفیف ویژه</p> "); 
-document.writeln("         </div> "); 
-document.writeln("         <button>خرید کنید</button> "); 
-document.writeln("     </footer> ");
-
