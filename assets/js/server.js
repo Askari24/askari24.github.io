51e4861e7e3ba34ce4cf8f309b51915aae7045db
Aerf <ABNUS-aref@hotmail.com>
2024-09-30 09:57:32 -0700

    Add files via upload

---
+++ assets/js/server.js
@@ -0,0 +1,9 @@
+
+document.writeln(" <article> "); 
+document.writeln("             <div class=\"column\"> "); 
+document.writeln("                 <p>نوشیدنی های سرد بر پایه قهوه</p> "); 
+document.writeln("                 <p>Coffee-based cold drinks</p> "); 
+document.writeln("                 <button href=\"\">دریافت</button> "); 
+document.writeln("             </div> "); 
+document.writeln("</article> ");
+
