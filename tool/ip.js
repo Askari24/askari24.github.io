c8907b7c185f67992d8838a40b33539da698bbf1
Aerf <ABNUS-aref@hotmail.com>
2025-04-04 14:16:50 +0330

    Delete tool directory

--- tool/ip.js
+++
@@ -1,8 +0,0 @@
-fetch('https://ipapi.co/json/') // استفاده از API رایگان برای دریافت اطلاعات IP
-.then(response => response.json())
-.then(data => {
-    if (data.country !== "IR","US") { // بررسی کشور
-        document.body.innerHTML = "<h1>دسترسی شما مسدود شده است!</h1>";
-    }
-})
-.catch(error => console.error('Error:', error));
